c34a2c2df05389df21dcc715467f5106f405b9c9
gropchy <donatellagropcaj@icloud.com>
2022-05-02 18:26:52 +0200

    Query3

--- EserciziAnagrafe.sql
+++ EserciziAnagrafe.sql
@@ -5,10 +5,13 @@ from Persona, Città
 where Persona.CittàNascita = Città.Nome and Persona.Età > 18 
 
 /* Query 2 - coppie <genitore,figlio> nati nella stessa città */
-use Anagrafe; 
 select PG.Nome
 from Genia join Persona PG on Genia.Genitore=PG.Nome
     join Persona PF on Genia.Figlio=PF.Nome
 where PG.CittàNascita = PF.CittàNascita
 
+/* Query 3 - città in cui è nato almeno un genitore di oltre 50 anni*/
+select distinct Genia.Genitore
+from Persona, Genia
+where Persona.Nome = Genia.Genitore and Persona.Età > 50
 
