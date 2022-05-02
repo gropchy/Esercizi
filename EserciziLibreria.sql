f9576673df7612fbf4ce8087512a97e031c88cf1
gropchy <donatellagropcaj@icloud.com>
2022-05-02 12:38:36 +0200

    commit esercizio2

--- EserciziLibreria.sql
+++ EserciziLibreria.sql
@@ -1,5 +1,10 @@
 
+/*1-Romanzi pubblicati nel 2009*/
 select Romanzo.Titolo
 from Romanzo
 where AnnoPubblicazione=2009
 
+/*2-Romanzi pubblicati a partire dal 1990*/
+select Romanzo.Titolo
+from Romanzo
+where AnnoPubblicazione>=1990
