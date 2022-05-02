d1e189aa009565e642ae7a63a14914043d072e43
gropchy <donatellagropcaj@icloud.com>
2022-05-02 12:45:03 +0200

    commit esercizio 3

--- EserciziLibreria.sql
+++ EserciziLibreria.sql
@@ -8,3 +8,10 @@ where AnnoPubblicazione=2009
 select Romanzo.Titolo
 from Romanzo
 where AnnoPubblicazione>=1990
+
+/*3-Romanzi del ventesimo secolo*/
+
+select Romanzo.Titolo
+from Romanzo
+where Romanzo.AnnoPubblicazione>=1901 and Romanzo.AnnoPubblicazione<=2000
+
