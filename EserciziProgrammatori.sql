aee92c19018e4f7fef418a6cb38123e953b1f7cc
gropchy <donatellagropcaj@icloud.com>
2022-05-03 16:30:01 +0200

    Query4

--- EserciziProgrammatori.sql
+++ EserciziProgrammatori.sql
@@ -1,34 +1,35 @@
 USE Programmatori;
 
-/* Query 1 - Trovare il codice dei programmatori che sono stati autori di
-almeno un programma scritto in Java dopo il 2000.*/
+/* Query 1 - Trovare il codice dei programmatori che sono stati autori di almeno un programma scritto in Java dopo il 2000.*/
 select Programmatore.codice
 from Autore join Programmatore on Programmatore.codice=Autore.codice
     join Programma on Programma.id=Autore.id
 where Programma.anno>2000 and Programma.linguaggio="Java";
 
-/* Query 2 - Trovare il nome e la categoria dei programmatori
-che sono stati autori di almeno un programma scritto
-in un linguaggio diverso da Python, ordinando il
-risultato rispetto al nome dei programmatori.*/
+/* Query 2 - Trovare il nome e la categoria dei programmatori che sono stati autori di almeno un programma scritto
+in un linguaggio diverso da Python, ordinando il risultato rispetto al nome dei programmatori.*/
 select Programmatore.nome, Programmatore.categoria
 from Autore join Programmatore on Programmatore.codice=Autore.codice
     join Programma on Programma.id=Autore.id
 where Programma.linguaggio!="Python";
 order by Programmatore.nome;
 
-/* Query 3 - Per ogni programmatore di categoria 10, Trovare il suo
-codice e l'anno in cui ha scritto il primo programma in un
-linguaggio diverso da Java.
-*/
+/* Query 3 - Per ogni programmatore di categoria 10, Trovare il suo codice e l'anno in cui ha scritto il primo programma in un linguaggio diverso da Java.*/
 select Programmatore.codice, Programma.anno
 from Autore join Programmatore on Programmatore.codice=Autore.codice
     join Programma on Programma.id=Autore.id
 where Programmatore.categoria=10 and Programma.linguaggio !="Java";
 
-/*Query 7 Per ogni linguaggio calcolare quanti sono gli
-autori dei programmi scritti in quel linguaggio.*/
-USE Programmatori;
+/* Query 4 - Trovare le coppie dei codici di programmatori che sono stati
+coautori di almeno un programma scritto in Python.
+*/
+select distinct Autore.codice as Autore, Autore2.codice as Coautore
+from Autore join Programma on Programma.id = Autore.id
+    join Autore Autore2 on Autore2.id = Programma.id
+where Programma.linguaggio = "Python" and Autore.codice != Autore2.codice;
+
+
+/*Query 7 Per ogni linguaggio calcolare quanti sono gli autori dei programmi scritti in quel linguaggio.*/
 select COUNT(Autore.codice), Programma.linguaggio
 from Autore join Programmatore on Programmatore.codice = Autore.codice
     join Programma on Programma.id = Autore.id
