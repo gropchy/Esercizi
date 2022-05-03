d04175d0beab8ccf201aa8a266dc0e5637d5aafa
gropchy <donatellagropcaj@icloud.com>
2022-05-03 12:41:35 +0200

    Query2

--- EserciziProgrammatori.sql
+++ EserciziProgrammatori.sql
@@ -7,3 +7,15 @@ from Autore join Programmatore on Programmatore.codice=Autore.codice
     join Programma on Programma.id=Autore.id
 where Programma.anno>2000 and Programma.linguaggio="Java";
 
+/* Query 2 - Trovare il nome e la categoria dei programmatori
+che sono stati autori di almeno un programma scritto
+in un linguaggio diverso da Python, ordinando il
+risultato rispetto al nome dei programmatori.*/
+USE Programmatori;
+select Programmatore.nome, Programmatore.categoria
+from Autore join Programmatore on Programmatore.codice=Autore.codice
+    join Programma on Programma.id=Autore.id
+where Programma.linguaggio!="Python";
+order by Programmatore.nome;
+
+
