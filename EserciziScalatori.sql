5c850e923205d31ca8aabb0dc4b5120f224b83d5
gropchy <donatellagropcaj@icloud.com>
2022-05-04 12:37:51 +0200

    Query2 aggiornata

--- EserciziScalatori.sql
+++ EserciziScalatori.sql
@@ -9,9 +9,10 @@ group by Scalata.scalatore, Nazione.continente;
 /* Query 2 - Per ogni scalatore nato prima del 1980, Trovare tutti i continenti in cui ha effettuato una scalata,
 ordinando il risultato per codice fiscale e, a parità di codice fiscale, per il nome del continente. */
 
-select distinct nazione.continente, scalata.scalatore
+select distinct nazione.continente, scalatore.cf
 from scalata join nazione on scalata.nazione=nazione.nome
     join scalatore on scalata.scalatore=scalatore.cf
 where scalatore.annoNascita<1980
 order by scalatore.cf, nazione.continente 
+
 
