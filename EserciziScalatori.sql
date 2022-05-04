00e92f94aae034b825ff85c4c8974eec04418199
gropchy <donatellagropcaj@icloud.com>
2022-05-04 12:45:57 +0200

    Query3

--- EserciziScalatori.sql
+++ EserciziScalatori.sql
@@ -9,10 +9,17 @@ group by Scalata.scalatore, Nazione.continente;
 /* Query 2 - Per ogni scalatore nato prima del 1980, Trovare tutti i continenti in cui ha effettuato una scalata,
 ordinando il risultato per codice fiscale e, a parità di codice fiscale, per il nome del continente. */
 
-select distinct nazione.continente, scalatore.cf
-from scalata join nazione on scalata.nazione=nazione.nome
-    join scalatore on scalata.scalatore=scalatore.cf
-where scalatore.annoNascita<1980
-order by scalatore.cf, nazione.continente 
+select distinct Nazione.continente, Scalatore.cf
+from Scalata join Nazione on Scalata.nazione=Nazione.nome
+    join Scalatore on Scalata.scalatore=Scalatore.cf
+where Scalatore.annoNascita<1980
+order by Scalatore.cf, Nazione.continente 
 
+/* Query 3 - Trovare le nazioni (mostrando, per ciascuna, anche il continente) nelle quali è stata effettuata almeno una scalata
+da uno scalatore con meno di 20 anni. */
+
+select distinct Nazione.nome, Nazione.continente
+from Scalata join Nazione on Scalata.nazione=Nazione.nome
+    join Scalatore on Scalata.scalatore=Scalatore.cf
+where Scalata.anno-Scalatore.annoNascita<20
 
