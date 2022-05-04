25783c404d65cf26265f92868e762fe0229b298a
gropchy <donatellagropcaj@icloud.com>
2022-05-04 12:59:47 +0200

    Query1 aggiornata

--- EserciziScalatori.sql
+++ EserciziScalatori.sql
@@ -1,10 +1,10 @@
 /* Query 1 - Trovare l'insieme (non il multi-insieme) delle coppie (A,B)
 tali che A è uno scalatore e B è un continente in cui A ha effettuato una scalata.*/
 
-select Scalata.scalatore, Nazione.continente
-from Scalata join Nazione on Scalata.nazione=Nazione.nome
-    join Scalatore on Scalata.scalatore=Scalatore.cf
-group by Scalata.scalatore, Nazione.continente;
+select distinct scalata.scalatore, nazione.continente
+from scalata join nazione on scalata.nazione=nazione.nome
+    join scalatore on scalata.scalatore=scalatore.cf
+order by scalata.scalatore
 
 /* Query 2 - Per ogni scalatore nato prima del 1980, Trovare tutti i continenti in cui ha effettuato una scalata,
 ordinando il risultato per codice fiscale e, a parità di codice fiscale, per il nome del continente. */
